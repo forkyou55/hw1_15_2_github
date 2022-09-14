d3d8be31b3250e417fb88439945cc7d330cb84dc
Stanislav Ivanishchev <stanislavivanishchev@MacBook-Air-Svetlana.local>
2022-09-14 22:52:43 +0200

    Init commit from console

---
+++ src/test/java/ExampleTest/ExampleTest.java
@@ -0,0 +1,14 @@
+package ExampleTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    void exampleTest() {
+        Assertions.assertTrue(2>1);
+    }
+
+
+}
